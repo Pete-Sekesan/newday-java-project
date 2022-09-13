7d7c07d6792487117e81835d405a1313d803cb17
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-09-13 10:51:40 -0400

    recommit of previous work

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+
+
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class App {
+
+    private static final String POSTS_API_URL = "https://daringfireball.net/feeds/json";
+
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("accept", "application/json")
+                .uri(URI.create(POSTS_API_URL))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+
+    }
+}
